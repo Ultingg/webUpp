99820098a4f65d8c4b39058f12fc61fd041cd987
ultinigg <ultinigg@gmail.com>
2022-11-10 19:59:07 +0300

    Add db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
+CREATE TABLE IF NOT EXISTS person(
+    id serial primary key,
+    first_name varchar(255),
+    last_name varchar(255)
+);
+
 INSERT INTO person (first_name, last_name)
 values
     ('Вася', 'Петров'),
