559d626250782cd156303c893d8e3f62a32ef084
ultinigg <ultinigg@gmail.com>
2022-11-10 19:06:33 +0300

    Add db

--- src/main/java/app/controller/MainController.java
+++ src/main/java/app/controller/MainController.java
@@ -1,14 +1,30 @@
 package app.controller;
 
+import app.dao.Person;
+import app.dao.PersonRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    private final PersonRepository personRepository;
+
+    public MainController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
 
     @GetMapping("/opa")
     public String getIndex() {
         return "opa";
+    }
+
+    @GetMapping("persons")
+    public ResponseEntity<List<Person>> getList(){
+       List<Person> list = personRepository.getList();
+       return ResponseEntity.ok(list);
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO person (first_name, last_name)
+values
+    ('Вася', 'Петров'),
+    ('David', 'Lapot')
+    ;
